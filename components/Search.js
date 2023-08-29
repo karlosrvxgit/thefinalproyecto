a857b94ed3362c65a47431a5215c62829c3cd8b7
Carlos Rojas Vega <karlosrvxx@gmail.com>
2023-08-28 21:58:54 -0500

    updates iii

--- components/Search.js
+++ components/Search.js
@@ -1,34 +1,30 @@
-'use client'
+"use client";
 // import Image from 'next/image'
 // import styles from './SideBar.css'
-import { useState } from 'react';
-import React from 'react'
-
-
+import { useState } from "react";
+import React from "react";
 
 export default function Search() {
-    const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
   return (
     <div>
-    <input id='inputmain'
-      type="text"
-      placeholder="Search for places"
-      // value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
-    />
-    <button id='buttons' >s</button>
-    <div>
-      {searchResults.length > 0 ? (
-        searchResults.map((result) => <p key={result}>{result}</p>)
-      ) : (
-        <p></p>
-      )}
+      <input
+        id="inputmain"
+        type="text"
+        placeholder="Search for places"
+       
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button id="buttons">s</button>
+      <div>
+        {searchResults.length > 0 ? (
+          searchResults.map((result) => <p key={result}>{result}</p>)
+        ) : (
+          <p></p>
+        )}
+      </div>
     </div>
-  </div>
-
-    
-  )
+  );
 }
- 
